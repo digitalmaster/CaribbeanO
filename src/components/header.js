347c183c8e25fab07ba7ade4c11774b474b65f80
Jose Browne <josebrowne@gmail.com>
2018-10-21 14:57:37 -0700

    Checkpoint: Add account login link

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,4 @@
+/* global jQuery */
 import React from "react";
 import { Link } from "gatsby";
 
@@ -5,34 +6,54 @@ import Logo from "../images/logo.png";
 
 const isActive = (path, currentPath) => (currentPath === path ? "current" : "");
 
-const Header = ({ location: { pathname: currentPage } }) => {
-  return (
-    <div id="outerheader">
-      <div className="container">
-        <header id="top" className="twelve columns">
-          <div id="logo" className="three columns alpha">
-            <a href="home.html">
-              <img src={Logo} alt="Logo" />
-            </a>
-          </div>
-          <section id="navigation" className="nine columns omega">
-            <nav>
-              <ul id="topnav" className="sf-menu">
-                <li className={isActive("/", currentPage)}>
-                  <Link to="/">Home</Link>
-                </li>
-                <li className={isActive("/about", currentPage)}>
-                  <Link to="/about">About</Link>
-                </li>
-              </ul>
-            </nav>
+class Header extends React.Component {
+  componentDidMount = () => {
+    // Prevent running on build
+    if (typeof window !== "undefined") {
+      jQuery("a[rel^='prettyPhoto']").prettyPhoto();
+    }
+  };
+
+  render = () => {
+    const {
+      location: { pathname: currentPage }
+    } = this.props;
+    return (
+      <div id="outerheader">
+        <div className="container">
+          <header id="top" className="twelve columns">
+            <div id="logo" className="three columns alpha">
+              <a href="home.html">
+                <img src={Logo} alt="Logo" />
+              </a>
+            </div>
+            <section id="navigation" className="nine columns omega">
+              <nav>
+                <ul id="topnav" className="sf-menu">
+                  <li className={isActive("/", currentPage)}>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.myaccountcenter.net/account/reseller/english/login.asp?iframe=true&width=100%&height=100%"
+                      rel="prettyPhoto[iframes]"
+                    >
+                      Account Login
+                    </a>
+                  </li>
+                  <li className={isActive("/about", currentPage)}>
+                    <Link to="/about">About Us</Link>
+                  </li>
+                </ul>
+              </nav>
+              <div className="clear" />
+            </section>
             <div className="clear" />
-          </section>
-          <div className="clear" />
-        </header>
+          </header>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+}
 
 export default Header;
